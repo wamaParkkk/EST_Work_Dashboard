34ef0050de664380bdd5d754efd8dea2d55413c1
Hyunsoo Park <zahir84@nate.com>
2025-08-08 17:24:41 +0900

    25/08/08 update

--- EST_Work_Dashboard/Program.cs
+++ EST_Work_Dashboard/Program.cs
@@ -19,13 +19,13 @@ builder.WebHost.ConfigureKestrel(options =>
 var app = builder.Build();
 
 // 서버 실행(exe)시 자동 브라우저 열기 (서버 PC에서만)
-
+/*
 System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
 {
     FileName = "http://localhost:5020",
     UseShellExecute = true
 });
-
+*/
 
 app.UseStaticFiles();
 
